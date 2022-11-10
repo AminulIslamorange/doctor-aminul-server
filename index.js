e8c84be31baac314458adcec93882052c33c70f2
AminulIslamorange <aminulislamorange@gmail.com>
2022-11-10 11:38:02 +0600

    mongodb add commit

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ app.use(cors())
 const jwt = require("jsonwebtoken")
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const auth = require("./auth")
+const uri = process.env.DB_URI
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+app.get('/', (req, res) => {
+    res.send("its working")
+})
